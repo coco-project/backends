cc0f96772e79438474b3ca4040ca3ebe214fa47c
Michel Käser <mail@michelkaeser.ch>
2015-07-13 19:41:18 +0200

    pyflake8 fixes (ignoring D100/D200)

--- src/ipynbsrv/backends/storage_backends.py
+++ src/ipynbsrv/backends/storage_backends.py
@@ -4,30 +4,31 @@ from ipynbsrv.contract.errors import DirectoryNotFoundError, StorageBackendError
 
 
 class LocalFileSystem(StorageBackend):
-    '''
-    Storage backend implementation using the local filesystem as the underlaying backend.
-    '''
 
-    '''
-    :inherit
-    '''
+    """
+    Storage backend implementation using the local filesystem as the underlaying backend.
+    """
+
     def __init__(self, base_dir):
+        """
+        :inherit.
+        """
         super(LocalFileSystem, self).__init__(base_dir)
         self._fs = FileSystem(base_dir)
 
-    '''
-    :inherit
-    '''
     def dir_exists(self, dir_name, **kwargs):
+        """
+        :inherit.
+        """
         try:
             return self._fs.exists(dir_name) and self._fs.is_dir(dir_name)
         except Exception as ex:
             raise StorageBackendError(ex)
 
-    '''
-    :inherit
-    '''
     def get_dir_group(self, dir_name, **kwargs):
+        """
+        :inherit.
+        """
         if not self.dir_exists(dir_name):
             raise DirectoryNotFoundError("Directory does not exist.")
 
@@ -36,10 +37,10 @@ class LocalFileSystem(StorageBackend):
         except Exception as ex:
             raise StorageBackendError(ex)
 
-    '''
-    :inherit
-    '''
     def get_dir_mode(self, dir_name, **kwargs):
+        """
+        :inherit.
+        """
         if not self.dir_exists(dir_name):
             raise DirectoryNotFoundError("Directory does not exist.")
 
@@ -48,10 +49,10 @@ class LocalFileSystem(StorageBackend):
         except Exception as ex:
             raise StorageBackendError(ex)
 
-    '''
-    :inherit
-    '''
     def get_dir_owner(self, dir_name, **kwargs):
+        """
+        :inherit.
+        """
         if not self.dir_exists(dir_name):
             raise DirectoryNotFoundError("Directory does not exist.")
 
@@ -60,10 +61,10 @@ class LocalFileSystem(StorageBackend):
         except Exception as ex:
             raise StorageBackendError(ex)
 
-    '''
-    :inherit
-    '''
     def get_full_dir_path(self, dir_name, **kwargs):
+        """
+        :inherit.
+        """
         if not self.dir_exists(dir_name):
             raise DirectoryNotFoundError("Directory does not exist.")
 
@@ -72,22 +73,22 @@ class LocalFileSystem(StorageBackend):
         except Exception as ex:
             raise StorageBackendError(ex)
 
-    '''
-    :inherit
-    '''
     def mk_dir(self, dir_name, **kwargs):
+        """
+        :inherit.
+        """
         try:
             self._fs.mk_dir(dir_name)
         except Exception as ex:
             raise StorageBackendError(ex)
 
-    '''
-    :inherit
-
-    :param recursive: If true, recursively remove the dir.
-                      If false and the directory is not empty, an error is raised.
-    '''
     def rm_dir(self, dir_name, **kwargs):
+        """
+        :inherit.
+
+        :param recursive: If true, recursively remove the dir.
+                          If false and the directory is not empty, an error is raised.
+        """
         if not self.dir_exists(dir_name):
             raise DirectoryNotFoundError("Directory does not exist.")
 
@@ -100,10 +101,10 @@ class LocalFileSystem(StorageBackend):
         except Exception as ex:
             raise StorageBackendError(ex)
 
-    '''
-    :inherit
-    '''
     def set_dir_group(self, dir_name, group, **kwargs):
+        """
+        :inherit.
+        """
         if not self.dir_exists(dir_name):
             raise DirectoryNotFoundError("Directory does not exist.")
 
@@ -112,10 +113,10 @@ class LocalFileSystem(StorageBackend):
         except Exception as ex:
             raise StorageBackendError(ex)
 
-    '''
-    :inherit
-    '''
     def set_dir_mode(self, dir_name, mode, **kwargs):
+        """
+        :inherit.
+        """
         if not self.dir_exists(dir_name):
             raise DirectoryNotFoundError("Directory does not exist.")
 
@@ -124,10 +125,10 @@ class LocalFileSystem(StorageBackend):
         except Exception as ex:
             raise StorageBackendError(ex)
 
-    '''
-    :inherit
-    '''
     def set_dir_owner(self, dir_name, owner, **kwargs):
+        """
+        :inherit.
+        """
         if not self.dir_exists(dir_name):
             raise DirectoryNotFoundError("Directory does not exist.")
 
