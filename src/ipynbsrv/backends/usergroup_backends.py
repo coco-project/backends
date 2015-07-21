64e9f72f5f0ebb684e36935155aee219927b853a
Christian Glatthard <christian.glatthard@students.fhnw.ch>
2015-07-21 02:34:52 +0200

    fix removing user from all groups

--- src/ipynbsrv/backends/usergroup_backends.py
+++ src/ipynbsrv/backends/usergroup_backends.py
@@ -84,6 +84,8 @@ class LdapBackend(GroupBackend, UserBackend):
         else:
             username = dn
 
+        print(self.server)
+
         try:
             self.cnx = ldap.initialize(self.server)
             self.cnx.bind_s(str(username), str(credentials.get('password')))
@@ -188,9 +190,12 @@ class LdapBackend(GroupBackend, UserBackend):
         if not self.user_exists(user):
             raise UserNotFoundError
 
-        dn = self.get_full_user_dn(user)
+        dn = self.get_full_user_dn(str(user))
         try:
-            self.remove_user_from_all_groups(user)
+            self.remove_user_from_all_groups(str(user))
+        except:
+            pass
+        try:
             self.cnx.delete_s(str(dn))
         except BackendError as ex:
             raise ex
@@ -415,7 +420,7 @@ class LdapBackend(GroupBackend, UserBackend):
         if self.readonly:
             raise ReadOnlyError
         if not self.user_exists(user):
-            raise UserNotFoundError
+            raise UserNotFoundError(user)
 
         for group in self.get_groups():
             if self.is_group_member(group.get(GroupBackend.FIELD_PK), user):
