526f44f8e2f270e01403070a99819e4a3bae2d2a
Michel Käser <mail@michelkaeser.ch>
2015-07-25 13:39:08 +0200

    upgraded deps containing bug fixes

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     package_dir={'': 'src'},
     namespace_packages=['ipynbsrv'],
     install_requires=[
-        'docker-py==1.3.0',
+        'docker-py==1.3.1',
         'ipynbsrv-contract',
         'requests==2.7.0',
-        'python-ldap==2.4.19'
+        'python-ldap==2.4.20'
     ],
 )
